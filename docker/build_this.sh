e785f1f4a9a216547065cd55b21f82ae8468ce8f
edoardesd <edoardesd@gmail.com>
2020-09-21 13:48:16 +0200

    auto script for docker

---
+++ docker/build_this.sh
@@ -0,0 +1,7 @@
+version=$1 
+docker build --no-cache -t flipperthedog/go_publisher:$version .
+container_id=$(docker run -it --rm --detach flipperthedog/go_publisher:$version)
+docker exec -it $container_id apk add bash
+docker commit $container_id
+docker push flipperthedog/go_publisher:$version
+docker rm -f $container_id
