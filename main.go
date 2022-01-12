cf42ad91b3fbe67707fb8dab5ee0a797bca3f02a
edoardesd <edoardesd@gmail.com>
2022-01-12 17:39:02 +0100

    add milli

--- main.go
+++ main.go
@@ -91,7 +91,7 @@ func main() {
 
 	for i := 0; i < *clients; i++ {
 		if !*quiet {
-			log.Println("Starting cliente ", i)
+			log.Println("Starting clientz ", i)
 		}
 		c := &Client{
 			ID:         i,
